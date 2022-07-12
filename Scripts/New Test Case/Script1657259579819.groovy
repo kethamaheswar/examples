b2c76c21d8745e1afbf54d066dc436466691d4c6
Vooha-QualiTlabs <91185844+Vooha-QualiTlabs@users.noreply.github.com>
2022-07-12 10:46:29 +0530

    dmeo

---
+++ Scripts/New Test Case/Script1657259579819.groovy
@@ -0,0 +1,33 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\LENOVO\\Katalon Studio\\MobileTesting\\androidapp\\APIDemos.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView - Animation'), 0)
+
+Mobile.pressBack()
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView - App'), 0)
+
+Mobile.takeScreenshot('C:\\Users\\LENOVO\\AppData\\Local\\Temp\\screenshot4445556362022554392.png')
+
+Mobile.pressBack()
+
+Mobile.closeApplication()
+
